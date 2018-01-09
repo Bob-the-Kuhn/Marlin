ab928b07c0189e5b1177bde406ff6a8464b40e59
Bob-the-Kuhn <bob.kuhn@att.net>
2018-01-09 17:51:01 -0600

    snap

--- Marlin/src/HAL/SPI_manager_api.h
+++ Marlin/src/HAL/SPI_manager_api.h
@@ -8,15 +8,29 @@ namespace HAL {
 namespace SPI {
 
 /**
- * spi_channel
- *  0 - software SPI
- *  1 - hardware controller used for LCD's SD card
- *  2+ - system dependent
+ * create_logical_spi_channel 
+ *
+ *  returns logical channel number, -1 if out of channels
+ *
+ *  spi_channel
+ *   1 - hardware controller used for LCD's SD card
+ *   2+ - hardware controller, system dependent
+ *
+ *  cs_pin -  chip select pin, -1 if app will control chip select
+ *  cs_polarity - 1 if active high, 0 if active lower_bound
+ *  frequency in Hz for the SPI clock
+ *  spi_mode - 0-3
+ *  SCK (software SPI only)
+ *  MOSI (software SPI only)
+ *  MISO (software SPI only)
+ *
  */
 
-void set_frequency(int8_t channel, uint32_t frequency);
 
+// hardware SPI
 int8_t create_logical_spi_channel(int8_t spi_channel, pin_t cs_pin, bool cs_polarity, uint32_t frequency, uint8_t spi_mode);
+
+// software SPI
 int8_t create_logical_spi_channel(pin_t SCK, pin_t MOSI, pin_t MISO, pin_t cs, bool cs_polarity, uint32_t frequency, uint8_t spi_mode);
 
 void read(uint8_t channel, uint8_t *buffer, uint32_t length);
@@ -28,7 +42,8 @@ void write(uint8_t channel, uint8_t value);
 void transfer(uint8_t channel, const uint8_t *buffer_write, uint8_t *buffer_read, uint32_t length);
 uint8_t transfer(uint8_t channel, uint8_t value);
 
-
+// change SPI frequency for specified channel
+void set_frequency(int8_t channel, uint32_t frequency);
 
 
 }
